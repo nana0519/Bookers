b4c2312c5e6c87f68c395323a568f3173854743e
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-01-13 07:57:27 +0000

    [Update]機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,33 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
+    @books = Book.all
   end
   
   def create
-    list = Book.new(book_params)
-    list.save
-    redirect_to book_path(list.id)
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'  
   end
   
   private
